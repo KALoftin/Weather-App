4d26fe19ab54b7f4477a40b1a65a0cf447872209
Kyle Loftin <kaloftin85@gmail.com>
2023-05-23 14:18:50 -0700

    added secure environments

---
+++ weatherApp/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  baseURL: 'https://open-weather13.p.rapidapi.com/city/',
+  reverseGeoCodeURL:
+    'https://api.bigdatacloud.net/data/reverse-geocode-client?',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: 'YOUR KEY VALUE',
+};
